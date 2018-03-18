d6eece9da1212ce40b78a2b2733bcd2724fbd725
zhoushengfan <shengfan.zhou@amttgroup.com>
2018-03-18 11:39:57 +0800

    修改注册中心逻辑

---
+++ rpc-common/src/main/java/com/rpc/common/util/Holder.java
@@ -0,0 +1,19 @@
+package com.rpc.common.util;
+
+/**
+ *
+ * @author zhoushengfan
+ */
+public class Holder<T> {
+
+    private volatile T value;
+
+    public void set(T value) {
+        this.value = value;
+    }
+
+    public T get() {
+        return value;
+    }
+
+}
--- rpc-common/src/main/java/com/rpc/common/util/RegistryUtil.java
+++ rpc-common/src/main/java/com/rpc/common/util/RegistryUtil.java
@@ -5,8 +5,12 @@ import com.rpc.common.domain.URL;
 import com.rpc.common.exception.SimpleRpcExMsgConstants;
 import com.rpc.common.exception.SimpleRpcException;
 import com.rpc.common.registry.Registry;
+import com.rpc.common.registry.RegistryFactory;
 import com.rpc.common.registry.support.local.LocalRegistryFactory;
 import com.rpc.common.registry.support.zookeeper.ZookeeperRegistryFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created on 2017/10/1.
@@ -15,24 +19,27 @@ import com.rpc.common.registry.support.zookeeper.ZookeeperRegistryFactory;
  */
 public class RegistryUtil {
 
-    public static Registry resolveRegistryType(URL url){
+    private static final Holder<Map<String, Class<? extends RegistryFactory>>> registryClasses = new Holder<>();
+
+    static {
+        registryClasses.set(new HashMap<>());
+        registryClasses.get().put("zookeeper", ZookeeperRegistryFactory.class);
+        registryClasses.get().put("local", LocalRegistryFactory.class);
+    }
+
+    public static Registry resolveRegistryType(URL url) throws IllegalAccessException, InstantiationException {
         if (url == null){
             String registryUrl = GlobalCfgParam.RegistryAddress.getStrVal();
             url = URL.toURL(registryUrl);
         }
         String protocol = url.getProtocol();
-        Registry registry;
-        switch (protocol){
-            case SimpleRpcConstants.ZK_DEFAULT_PREFIX:
-                registry = new ZookeeperRegistryFactory().getRegistry(url);
-                break;
-            case SimpleRpcConstants.LOCAL_DEFAULT_PREFIX:
-                registry = new LocalRegistryFactory().getRegistry(url);
-                break;
-            default:
-                throw new SimpleRpcException("Unsupported registry type", SimpleRpcExMsgConstants.RPC_UNKNOWN_REGISTRY_ERROR);
+        Class<? extends RegistryFactory> registryType = registryClasses.get().get(protocol);
+        if (registryType == null) {
+            throw new SimpleRpcException("Unsupported registry type", SimpleRpcExMsgConstants.RPC_UNKNOWN_REGISTRY_ERROR);
         }
 
-        return registry;
+        RegistryFactory factory = registryType.newInstance();
+
+        return factory.getRegistry(url);
     }
 }
