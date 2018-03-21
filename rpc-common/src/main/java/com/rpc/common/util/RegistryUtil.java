e5893ad5da1b58f3cd0836818c344329beb86176
zhoushengfan <shengfan.zhou@amttgroup.com>
2018-03-21 23:18:59 +0800

    注册中心改为SPI加载机制

--- rpc-common/src/main/java/com/rpc/common/util/RegistryUtil.java
+++ rpc-common/src/main/java/com/rpc/common/util/RegistryUtil.java
@@ -10,7 +10,9 @@ import com.rpc.common.registry.support.local.LocalRegistryFactory;
 import com.rpc.common.registry.support.zookeeper.ZookeeperRegistryFactory;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.ServiceLoader;
 
 /**
  * Created on 2017/10/1.
@@ -22,9 +24,7 @@ public class RegistryUtil {
     private static final Holder<Map<String, Class<? extends RegistryFactory>>> registryClasses = new Holder<>();
 
     static {
-        registryClasses.set(new HashMap<>());
-        registryClasses.get().put("zookeeper", ZookeeperRegistryFactory.class);
-        registryClasses.get().put("local", LocalRegistryFactory.class);
+        loadRegistry();
     }
 
     public static Registry resolveRegistryType(URL url) throws IllegalAccessException, InstantiationException {
@@ -41,5 +41,26 @@ public class RegistryUtil {
         RegistryFactory factory = registryType.newInstance();
 
         return factory.getRegistry(url);
+    }
+
+    private static String simplifyFactoryName(String originName) {
+        String factorySuffix = "RegistryFactory";
+        if (originName.contains(factorySuffix)) {
+            originName = originName.substring(0, originName.indexOf(factorySuffix));
+            originName = originName.substring(0, 1).toLowerCase() + originName.substring(1);
+        }
+
+        return originName;
+    }
+
+    private static void loadRegistry() {
+        if (registryClasses.get() == null) {
+            registryClasses.set(new HashMap<>(4));
+        }
+        ServiceLoader<RegistryFactory> registryFactoryLoader = ServiceLoader.load(RegistryFactory.class);
+        for (RegistryFactory registryFactory : registryFactoryLoader) {
+            Class factoryClass = registryFactory.getClass();
+            registryClasses.get().put(simplifyFactoryName(factoryClass.getSimpleName()), factoryClass);
+        }
     }
 }
